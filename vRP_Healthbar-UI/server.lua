f1c91320ef7d24fa4cae1de253dd3d27001cbfba
Terbium <39789348+TerbSEC@users.noreply.github.com>
2019-06-29 16:58:10 +0100

    Delete server.lua

--- vRP_Healthbar-UI/server.lua
+++
@@ -1,8 +0,0 @@
-local Proxy = module("vrp", "lib/Proxy")
-vRP = Proxy.getInterface("vRP")
-
-RegisterServerEvent("vRP_HealthUI:getData")
-AddEventHandler("vRP_HealthUI:getData", function ()
-    local user_id = vRP.getUserId({source})
-    TriggerClientEvent("vRP_HealthUI:returnBasics",source,vRP.getHunger({user_id}),vRP.getThirst({user_id}))
-end)
