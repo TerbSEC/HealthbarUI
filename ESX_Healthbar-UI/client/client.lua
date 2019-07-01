5d06f7f28097e7fa758cb83913d5ad9f4271209a
Niklas <33189849+EnVyyyyy@users.noreply.github.com>
2019-07-01 17:51:38 +0200

    Update client.lua

--- ESX_Healthbar-UI/client/client.lua
+++ ESX_Healthbar-UI/client/client.lua
@@ -1,27 +1,26 @@
 local status = nil
 Citizen.CreateThread(function()
     while true do
-
-        Citizen.Wait(250)
-        local xPlayer = PlayerPedId()
-       
+        Citizen.Wait(100)
+        
         SendNUIMessage({
             show = IsPauseMenuActive(),
-            health = GetEntityHealth(xPlayer)-100,
-            armor = GetPedArmour(xPlayer),
-            stamina = 100-GetPlayerSprintStaminaRemaining(PlayerId()),
+            health = GetEntityHealth(GetPlayerPed(-1)) - 100,
+            armor = GetPedArmour(GetPlayerPed(-1)),
+            stamina = 100 - GetPlayerSprintStaminaRemaining(PlayerId()),
             st = status,
             healthtext = cfg.healthtext,
             armortext = cfg.armortext,
+            deadtext = cfg.deadtext
         })
     end
 end)
-
 
 RegisterNetEvent('ESX_HealthBAR-UI:updateStatus')
 AddEventHandler('ESX_HealthBAR-UI:updateStatus', function(Status)
     status = Status
     SendNUIMessage({
+        action = "updateStatus",
         st = Status,
     })
 end)
