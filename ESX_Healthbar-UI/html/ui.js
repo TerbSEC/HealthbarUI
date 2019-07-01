4dd09bd44ff38398d76a4fb7254c34a0c8e8eb17
Niklas <33189849+EnVyyyyy@users.noreply.github.com>
2019-07-01 17:52:17 +0200

    Update ui.js

--- ESX_Healthbar-UI/html/ui.js
+++ ESX_Healthbar-UI/html/ui.js
@@ -1,37 +1,23 @@
 $(document).ready(function() {
     window.addEventListener('message', function(event) {
         var data = event.data;
-        $(".container").css("display",data.show? "none":"block");
-        $("#boxStamina").css("width",data.stamina + "%");
-        $('#boxHunger').css('width', event.data.st[0].percent+'%')
-        $('#boxThirst').css('width', event.data.st[1].percent+'%')
+        $(".container").css("display", data.show? "none":"block");
+        $("#boxStamina").css("width", data.stamina + "%");
 
-        if (data.armor == 0) {
-            $("#boxHeal").css("width",data.health + "%"); // SHOW NORMAL BAR
-            $('#boxHeal2').hide(); // HIDE 2ND BAR
-            $('#heal2').hide(); // HIDE 2ND BAR
-            $('#boxArmor').hide(); // HIDE 2ND BAR
-            $('#armor').hide(); // HIDE 2ND BAR
+        if (event.data.action == "updateStatus") {
+            updateStatus(event.data.st);
+        }
 
-            $('#heal').show(); // HIDE 2ND BAR
-            $('#boxHeal').show(); // HIDE 2ND BAR
+        if (data.armor > 0 ) {
 
-
-            $('.armornumber').hide();
-            $('.healthnumber2').hide();
-
-            if (data.healthtext == true) {
-                $('.healthnumber').show();
-                $('.healthnumber').text(event.data.health);
-            }
-        } else {
-            $("#boxHeal2").css("width",data.health + "%"); // SHOW NORMAL BAR
-            $("#boxArmor").css("width",data.armor + "%"); // SHOW NORMAL BAR
-            $('#boxHeal').hide(); // HIDE 2ND BAR
+            $("#boxHeal2").css("width", data.health + "%"); // SHOW NORMAL BAR
+            $("#boxArmor").css("width", data.armor + "%"); // SHOW NORMAL BAR
             $('#heal').hide(); // HIDE 2ND BAR
+            $('#boxHeal').hide(); // HIDE 2ND BAR
 
             $('#heal2').show(); // HIDE 2ND BAR
             $('#boxHeal2').show(); // HIDE 2ND BAR
+
             $('#boxArmor').show(); // HIDE 2ND BAR
             $('#armor').show(); // HIDE 2ND BAR
             $('.healthnumber').hide();
@@ -45,9 +31,37 @@ $(document).ready(function() {
                 $('.armornumber').show();
                 $('.armornumber').text(event.data.armor);
             }
+        } 
+    else
+        if (data.armor == 0 ) {
+            $("#boxHeal").css("width", data.health + "%"); // SHOW NORMAL BAR
+            $('#boxHeal2').hide(); // HIDE 2ND BAR
+            $('#heal2').hide(); // HIDE 2ND BAR
+
+            $('#boxArmor').hide(); // HIDE 2ND BAR
+            $('#armor').hide(); // HIDE 2ND BAR
+
+            $('#heal').show(); // HIDE 2ND BAR
+            $('#boxHeal').show(); // HIDE 2ND BAR
+
+
+            $('.armornumber').hide();
+            $('.healthnumber2').hide();
+
+            if (data.healthtext == true) {
+                $('.healthnumber').show();
+                $('.healthnumber').text(event.data.health);
+                if (data.health < 0 ) {
+                    $('.healthnumber').text(data.deadtext);
+                }
+            }
 
 
         }
     })
 })
 
+function updateStatus(status){
+    $('#boxHunger').css('width', status[0].percent+'%')
+    $('#boxThirst').css('width', status[1].percent+'%')
+}
